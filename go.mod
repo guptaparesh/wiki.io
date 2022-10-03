910358cdb2ce51e81746725d1a613f0f914eeec2
Paresh Gupta <guptaparesh@gmail.com>
2022-10-03 12:40:38 -0700

    wiki-search http server with gorilla/mux

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require git.mills.io/prologic/bitcask v1.0.2
 require (
 	github.com/abcum/lcp v0.0.0-20201209214815-7a3f3840be81 // indirect
 	github.com/gofrs/flock v0.8.0 // indirect
+	github.com/gorilla/mux v1.8.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/plar/go-adaptive-radix-tree v1.0.4 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
--- kvpstore/kvpstore.go
+++ kvpstore/kvpstore.go
@@ -16,19 +16,6 @@ func SetupDb() {
 	}
 }
 
-func ExampleNgw(k string, v string) string {
-	SetupDb()
-    defer Cleanup()
-
-    DB.Put([]byte(k), []byte(v))
-    val, err := DB.Get([]byte(k))
-	if err != nil {
-		log.Fatalf("error %v\n", err)
-	}
-    //log.Printf(string(val))
-	return string(val)
-}
-
 func Example(k string, v string) string {
 	db, err := bitcask.Open("/tmp/db")
     defer db.Close()
@@ -47,9 +34,20 @@ func Cleanup() error {
 }
 
 func GetFromCache(key string) []byte {
+	return GetIfExists(DB, key)
+}
+
+func Put(key string, val []byte) {
+	PutInDb(DB, key, val)
+} 
+
+func GetIfExists(db *bitcask.Bitcask, key string) []byte {
 	k := []byte(key)
-	if DB.Has(k) {
-		val, err := DB.Get(k)
+	if db == nil {
+		log.Fatalln("non nil DB is required")
+	}
+	if db.Has(k) {
+		val, err := db.Get(k)
 		if err != nil {
 			log.Fatalf("error %v\n", err)
 		}
@@ -58,9 +56,9 @@ func GetFromCache(key string) []byte {
 	return nil	
 }
 
-func Put(key string, val []byte) {
+func PutInDb(db *bitcask.Bitcask, key string, val []byte) {
 	k := []byte(key)
-	err := DB.Put(k, val)
+	err := db.Put(k, val)
 	if err != nil {
 		log.Println("Error putting", key, "in cache")
 	}
